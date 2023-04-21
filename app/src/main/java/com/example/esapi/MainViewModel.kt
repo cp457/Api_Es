9855110b82b06696c7afb22aa822592f6ebb5c96
cp457 <christianpriolo96@gmail.com>
2023-04-21 11:34:27 +0200

    Kotlin Flow!

--- app/src/main/java/com/example/esapi/MainViewModel.kt
+++ app/src/main/java/com/example/esapi/MainViewModel.kt
@@ -1,34 +1,39 @@
 package com.example.esapi
 
+import android.content.SharedPreferences
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.launch
+import org.intellij.lang.annotations.Flow
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+
+
+
 
 class MainViewModel : ViewModel() {
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl("https://dog.ceo/%22")
         .addConverterFactory(GsonConverterFactory.create()).build()
 
-    private val apiService: ApiServiceInterface = retrofit.create(ApiServiceInterface::class.java)
-    private var dog = MutableLiveData<RetrofitData>()
-    val dog2: LiveData<RetrofitData>
-        get() = dog
 
-    private var error = MutableLiveData<String>()
-    val error2: LiveData<String>
-        get() = error
+
+    private val apiService: ApiServiceInterface = retrofit.create(ApiServiceInterface::class.java)
+    private var dog = MutableSharedFlow<RetrofitData>()
+
+    private var error = MutableSharedFlow<String>()
+
 
     fun dogApi() {
         CoroutineScope(Dispatchers.Main).launch {
             try {
-                dog.value = apiService.getData()
+                dog.emit( apiService.getData())
             } catch (e: Exception) {
-                error.value = e.localizedMessage
+                error.emit(e.localizedMessage)
             }
         }
     }
--- app/src/main/java/com/example/esapi/preferencies/SharedPreferencies.kt
+++
@@ -1,7 +0,0 @@
-package com.example.esapi.preferencies
-
-import android.content.Context
-import android.provider.Settings.Global.getString
-import com.example.esapi.R
-
-
